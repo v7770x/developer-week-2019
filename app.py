608f01b60a8c3c726e26a6e9b23ed6a1c10b301c
Vishvam Mazumdar <vishvam.mazumdar@gmail.com>
2019-02-23 19:31:51 -0800

    classifying and saving image data to db

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 import pyrebase
+# from clarifai.rest import ClarifaiApp
+import requests
+import json
+from requests.auth import HTTPBasicAuth
 app = Flask(__name__)
+# classifier = ClarifaiApp(api_key='53785869e72f4901a7f93891241cdc10')
+# model = classifier.public_models.general_model
+
 
 config = {
 	"apiKey": "AIzaSyAviP5BdXqcBr409cY78pu0goV60t_uofY",
@@ -29,7 +36,77 @@ def upload_image(local_pth):
 
 def get_url(pth):
     url = storage.child(pth).get_url(None)
+    print(url)
     return url
 
-print(get_url("images/test.jpg"))
+def classify_image(url):
+    data ='''
+    {
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": "'''+url+ '''"
+            }
+          }
+        }
+      ]
+    }'''
+    # data = '{\n     "inputs": [\n {\n   "data": { \n"image": {\n  "url":' +'https://en.wikipedia.org/wiki/Food#/media/File:Good_Food_Display_-_NCI_Visuals_Online.jpg'+'           }\n }\n   ]}'
+    # data = {
+    #     "inputs":[
+    #         {"data":}
+    #     ]
+    # }
+    headers = {'Authorization':'Key 53785869e72f4901a7f93891241cdc10', 'Content-type':'application/json'}
+    res= requests.post(url ="https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/versions/aa7f35c01e0642fda5cf400f543e7c40/outputs",
+        headers=headers,data=data)
+    # res = requests.post(url="https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/versions/aa7f35c01e0642fda5cf400f543e7c40/outputs", 
+    #     data=data,
+    #       headers= headers)
+    # string = res.read().decode('utf-8')
+    txt = res.json()
+    # print(txt.get('outputs'))
+    # print(type(txt))
+    raw_data = txt.get('outputs')[0].get('data').get('concepts')
+        # print(response)
+        # return url
+    return raw_data
+
+# url = get_url("images/test.jpg")
+# print(url)
+
+@app.route("/save_image", methods=["GET", "POST"])
+def save_image_props_to_database():
+    print("got")
+    # 
+    data = request.get_json(force=True)
+    user = data.get('user')
+    url = data.get('url')
+    latitude= data.get('latitude')
+    longitude = data.get('longitude')
+    classification = classify_image(url)[0].get('name')
+    print(user,url,latitude,longitude)
+    print(classification)
+    new_datum = {"user":user,"url": url, "location": {"latitude":latitude,"longitude":longitude},"classification":classification}
+    db.child("users").child(user).push(new_datum)
+
+    #check if its been added
+    users = db.child('users').get()
+    print(users.val())
+
+    return "saved"
+	# 
+	# users = db.child('users').get()
+	# print(users.val())
+	# db.child("users").push()  
+    # new_datum = {"user":user,"url": url, "location": {"latitude":latitude,"longitude":longitude}}
+    # db.child("users").child(user).push(new_datum)
+
+# save_image_props_to_database("test", url, 10,10)
+# json_obj = json.loads(res.text)
+# print(json_obj.get('data'))
+# classify_image(get_url("images/test.jpg"))
+
+# print(get_url("images/test.jpg"))
 # upload_image("images/test.jpg")
